6f8439e319ad5605cae3bd9e9c4213a206ad4084
Aman kumar <40682420+Duchies@users.noreply.github.com>
2025-01-28 22:18:35 +0530

    slice

---
+++ slice.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Main started..")
+    var arr []int
+    arr = append(arr,2)
+    var arr2 []int
+    
+    arr2 = append(arr2,7)
+    arr2 = append(arr2,9)
+
+    arr2 = append(arr2,arr...)
+    
+    fmt.Println(arr)
+    fmt.Println(arr2)
+
+    //learning is append only work with []int. not with [len]int
+    
+    var a [2]string
+  	a[0] = "Hello"
+  	a[1] = "World"
+  	fmt.Println(a[0], a[1])
+  	fmt.Println(a)
+  
+  	primes := [6]int{2, 3, 5, 7, 11, 13}
+  	fmt.Println(primes)
+
+    fmt.Println("Main ended...")
+}
