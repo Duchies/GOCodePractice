236a66b4459ed5d615da5abbb81897d20c07fff4
Aman kumar <40682420+Duchies@users.noreply.github.com>
2025-06-01 22:59:17 +0530

    Update Notes.go

--- Notes.go
+++ Notes.go
@@ -33,8 +33,26 @@ strings.ReplaceAll(s, "l", "L") // "heLLo worLd"
 
 point 4)
 
+# how to remove values from list, effective method.
+
+func main() {
+	list := []string{"Java", "Python", "Go", "JavaScript"}
+	removeIndex := 2
+	// Remove the element at index 2
+	newList := append(list[:removeIndex], list[removeIndex+1:]...)
+	// append allocate new memory and returns a new slice
+
+	fmt.Println(newList) // Output: [Java Python JavaScript]
+}
+
+
+
+point 5)
+
 result = append(result, list[itr++])  // ❌ Invalid in Go
 
 In Go, the itr++ syntax is called a statement, not an expression.
 A statement does something, like itr++ (it increases itr).
 An expression returns a value that you can use, like list[itr++] in Java or C.
+
+
