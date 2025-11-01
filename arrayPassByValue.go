b39f3876c8e8954321982046445ab9675ea284d4
Aman kumar <40682420+Duchies@users.noreply.github.com>
2025-11-01 13:24:42 +0530

    Implement list function to modify slice
    Added a list function to append a value to the slice.

--- arrayPassByValue.go
+++ arrayPassByValue.go
@@ -6,8 +6,15 @@ func main() {
     fmt.Println("Try programiz.pro")
 
     arr := make([]int, 0) // Create an empty slice
-    helper(&arr)          // Pass the slice pointer to the helper function
+    list(arr)
+    fmt.Println(arr)
+    
+    helper(&arr) // Pass the slice pointer to the helper function
     fmt.Println(arr)      // Print the modified slice
+}
+
+func list(arr []int){
+    arr = append(arr,9)
 }
 
 func helper(arr *[]int) {
@@ -15,3 +22,10 @@ func helper(arr *[]int) {
     *arr = append(*arr, 2)
     *arr = append(*arr, 3)
 }
+
+/*
+output
+Try programiz.pro
+[]
+[1 2 3]
+*/
